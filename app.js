72b2e27e11f3d12e83f2c04449a4f92fcc37ffbd
ramya208 <tramya19092006@gmail.com>
2026-07-05 15:55:33 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,38 @@
+
+let tasks = [];
+
+function message() {
+
+    let input = document.querySelector(".input-box input").value;
+
+
+    tasks.push(input);
+
+    displayTask();
+
+    document.querySelector(".input-box input").value = "";
+}
+
+function displayTask() {
+
+    let output = document.querySelector(".output");
+
+    output.innerHTML = "";
+
+    for (let i = 0; i < tasks.length; i++) {
+
+        output.innerHTML += `
+            <li>
+                <span>${tasks[i]}</span>
+                <p style="color:green;">completed</p>
+
+                <i class="ri-delete-bin-line delete" onclick="deleteTask(${i})"></i>
+            </li>
+        `;
+    }
+}
+
+function deleteTask(index) {
+    tasks.splice(index, 1);
+    displayTask();
+}
